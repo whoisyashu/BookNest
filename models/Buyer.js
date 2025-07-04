7edb39bd5d5e53f1b7def0cfcff1485394ab048e
whoisyashu <maheshwariy077@gmail.com>
2025-07-04 17:35:35 +0530

    Seller and Buyer password checker missing

--- models/Buyer.js
+++ models/Buyer.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const buyerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
   phone: { type: String },
+  password: { type: String, required: true },
   registrationDate: {
     type: Date,
     required: true,
@@ -10,4 +13,17 @@ const buyerSchema = new mongoose.Schema({
   },
   address: { type: Object }
 });
+
+// Hash password before saving
+buyerSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password method
+buyerSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
 module.exports = mongoose.model('Buyer', buyerSchema);
